9b422f9359d90dfda76de3475b68f11fe02087e2
Alok123456 <alokrai319@gmail.com>
2019-01-08 23:28:00 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule, LoadingController } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ContactsProvider } from '../providers/contacts/contacts';
+
+import { HttpModule } from '@angular/http';
+import { ContactPage } from '../pages/contact/contact';
+
+import { CallNumber } from '@ionic-native/call-number';
+import { Contacts } from '@ionic-native/contacts';
+import { EmailComposer } from '@ionic-native/email-composer';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ContactPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ContactPage
+  ],
+  providers: [
+    StatusBar,
+    LoadingController,
+    EmailComposer,
+    CallNumber,
+    Contacts,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ContactsProvider
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,86 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ContactsProvider } from '../../providers/contacts/contacts';
+import { ContactPage } from '../contact/contact';
+import { Contacts } from '@ionic-native/contacts';
+import { ToastController, LoadingController } from 'ionic-angular'
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  contacts: any;
+  contactList:any;
+  allcontacts: any;
+
+  constructor(private everybody: Contacts,private loadingController: LoadingController,private toastController: ToastController,private contactsProvider: ContactsProvider,public navCtrl: NavController) {
+
+  }
+
+  onContactClick(contact) {
+    this.navCtrl.push(ContactPage,{'contact':contact});
+  }
+
+  ionViewDidLoad() {
+    const loading = this.loadingController.create({
+      content: 'Loading Contacts, Please wait...'
+    });
+    loading.present();
+
+    this.contactList = this.everybody.find(["*"]);
+
+    if(this.contactList){
+      console.log(this.contactList);
+      loading.dismiss();
+    }
+    // this.contactsProvider.getContacts().subscribe((data) => {
+    //   let responsecontacts = data["results"];
+    //   this.contacts = this.addFavoriteProperty(responsecontacts);
+    //   this.allcontacts = this.addFavoriteProperty(responsecontacts);
+    //   loading.dismiss();
+    // });
+  }
+
+  // private addFavoriteProperty(contacts) {
+  //   contacts.forEach(function(contact){
+  //     contact.isFavorite = false;
+  //   });
+  //   return contacts;
+  // }
+
+  // filterContacts(event) {
+  //   let searchvalue = event.target.value;
+  //   if(searchvalue && searchvalue !== "") {
+  //     searchvalue = searchvalue.toLowerCase();
+  //     this.contacts = this.allcontacts.filter((contact) => {
+  //         let contactname = (`${contact.name.title} ${contact.name.first} ${contact.name.last}`).toLowerCase();
+  //         return (contactname.indexOf(searchvalue) > -1);
+  //     });
+  //   } else {
+  //     this.contacts = this.allcontacts;
+  //   }
+  // }
+
+  deleteContact(index) {
+    this.contacts.splice(index,1);
+    this.showToast('Contact has been deleted.');
+  }
+
+  private showToast(message: string) {
+    let toast = this.toastController.create({
+      'message': message,
+      'duration': 2000
+    });
+    toast.present();
+  }
+
+  addToFavorite(contact, slidingitem) {
+    this.contactsProvider.addToFavorite(contact);
+    contact.isFavorite = true;
+    slidingitem.close();
+    this.showToast('Contact has been added to favorites.');
+  }
+
+}
